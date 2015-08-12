4047e30f4453c70d4cd09cf7701895d3f9976431
Matt Eves <mattheweves@outlook.com>
2015-08-12 12:20:22 +0000

    Lesson 18 Completed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
 
+	before_action :authenticate_user!, :only =>[:new, :create]
 	def index
 		#@places = Place.all
 		@places = Place.order("name").page(params[:page]).per(2)
@@ -10,7 +11,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 
