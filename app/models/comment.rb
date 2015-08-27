b8b087323d88337ba7fa3341d211a94719b17b01
Matt Eves <mattheweves@outlook.com>
2015-08-27 03:55:59 +0000

    Lesson 43 Complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 
 	belongs_to :user
 	belongs_to :place
-
+	after_create :send_comment_email	
 
 	RATINGS = {
 
@@ -18,5 +18,8 @@ class Comment < ActiveRecord::Base
 		RATINGS.invert[self.rating]
 	end
 	
+	 def send_comment_email
+      NotificationMailer.comment_added(self).deliver
+	  end
 
 end
